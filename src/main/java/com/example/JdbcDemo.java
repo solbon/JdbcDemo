337d41103ffa434709407bb38c0d1ed1e24e6e83
Solbon Daribazaron <solbon@gmail.com>
2017-05-10 20:26:47 +0200

    Implementing RowMapper

--- src/main/java/com/example/JdbcDemo.java
+++ src/main/java/com/example/JdbcDemo.java
@@ -17,5 +17,8 @@ public class JdbcDemo {
 //        System.out.println(circle.getName());
 
         System.out.println(dao.getCircleCount());
+        System.out.println(dao.getCircleName(1));
+        System.out.println(dao.getCircleForId(1).getName());
+        System.out.println(dao.getAllCircles().get(0).getName());
     }
 }
--- src/main/java/com/example/dao/JdbcDaoImpl.java
+++ src/main/java/com/example/dao/JdbcDaoImpl.java
@@ -3,10 +3,12 @@ package com.example.dao;
 import com.example.model.Circle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 
 /**
  * Created by Solbon on 2017-05-10.
@@ -46,6 +48,21 @@ public class JdbcDaoImpl {
         return jdbcTemplate.queryForObject("select count(*) from circle", Integer.class);
     }
 
+    public String getCircleName(int circleId) {
+        String sql = "select name from circle where id = ?";
+        return jdbcTemplate.queryForObject(sql, String.class, 1);
+    }
+
+    public Circle getCircleForId(int circleId) {
+        String sql = "select * from circle where id = ?";
+        return jdbcTemplate.queryForObject(sql, new CircleMapper(), 1);
+    }
+
+    public List<Circle> getAllCircles() {
+        String sql = "select * from circle";
+        return jdbcTemplate.query(sql, new CircleMapper());
+    }
+
     public JdbcTemplate getJdbcTemplate() {
         return jdbcTemplate;
     }
@@ -62,4 +79,16 @@ public class JdbcDaoImpl {
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
+
+    private static final class CircleMapper implements RowMapper<Circle> {
+
+        @Override
+        public Circle mapRow(ResultSet rs, int rowNum) throws SQLException {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            Circle circle = new Circle(id, name);
+            return circle;
+        }
+    }
+
 }
